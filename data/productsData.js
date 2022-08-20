02e1bc46204f1dcc9f8b7d019eef0933f3ff2a92
Nhut Duong <hello@nhutduong.com>
2022-08-20 22:54:22 +0700

    🗃️ Add products and services data

---
+++ data/productsData.js
@@ -0,0 +1,27 @@
+const productsData = [
+  {
+    title: 'Hoa Tang Lễ',
+    description:
+      'Dịch Vụ Tang Lễ Hai Thư cung cấp các loại liễng và vòng hoa tươi phục vụ nhu cầu phúng điếu.',
+    imgSrc: '/static/images/hoa-tang-le.jpg',
+  },
+  {
+    title: 'Hoa Tươi Trang Trí',
+    description:
+      'Dịch Vụ Tang Lễ Hai Thư cung cấp các loại hoa tươi trang trí trên quan tài theo yêu cầu.',
+    imgSrc: '/static/images/hoa-tuoi-trang-tri.jpg',
+  },
+  {
+    title: 'Trống, Chinh & Cờ Tang',
+    description: 'Dịch Vụ Tang Lễ Hai Thư cung cấp các loại trống, chinh và cờ tang theo yêu cầu.',
+    imgSrc: '/static/images/trong-chinh-co-tang.jpg',
+  },
+  {
+    title: 'Rạp Che & Bàn Ghế',
+    description:
+      'Dịch Vụ Tang Lễ Hai Thư cho thuê rạp che, bàn ghế, ly tách và chén đũa phục vụ cho tang lễ.',
+    imgSrc: '/static/images/rap-che-ban-ghe.jpg',
+  },
+]
+
+export default productsData
---
+++ data/servicesData.js
@@ -0,0 +1,28 @@
+const servicesData = [
+  {
+    title: 'Nhạc Lễ',
+    description:
+      'Dịch Vụ Tang Lễ Hai Thư cung cấp dịch vụ nghi thức nhạc lễ phù hợp với văn hoá từng vùng miền và tôn giáo.',
+    imgSrc: '/static/images/nhac-le.jpg',
+  },
+  {
+    title: 'Xe Tang',
+    description:
+      'Dịch Vụ Tang Lễ Hai Thư cung cấp dịch vụ xe tang chuyên dụng đưa đám tang và dịch vụ đón thi hài từ Lâm Đồng đi các tỉnh và ngược lại.',
+    imgSrc: '/static/images/xe-tang.jpg',
+  },
+  {
+    title: 'Hoả Táng',
+    description:
+      'Dịch Vụ Tang Lễ Hai Thư cung cấp dịch vụ hoả táng trọn gói tại Lâm Đồng, các tỉnh miền Trung và Tây Nguyên.',
+    imgSrc: '/static/images/hoa-tang.jpg',
+  },
+  {
+    title: 'Cải Táng',
+    description:
+      'Dịch Vụ Tang Lễ Hai Thư cung cấp dịch vụ cải táng trọn gói bao gồm tiểu, quách… theo các đạo khác nhau.',
+    imgSrc: '/static/images/cai-tang.jpg',
+  },
+]
+
+export default servicesData

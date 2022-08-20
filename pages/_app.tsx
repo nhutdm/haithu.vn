3291ef5eadebe21a4a2e7e50240529f1830a3987
Nhut Duong <hello@nhutduong.com>
2022-08-20 22:59:04 +0700

    🍱 Add custom font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/tailwind.css'
 
+import '@fontsource/eb-garamond'
+import '@fontsource/oswald'
+
 import type { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [],
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        stack: {
+          50: '#f4f5f4',
+          100: '#e5e7e4',
+          200: '#ccd0ca',
+          300: '#959d93',
+          400: '#80897e',
+          500: '#616a5f',
+          600: '#4a5249',
+          700: '#3a423a',
+          800: '#30362f',
+          900: '#282c28',
+        },
+      },
+      fontFamily: {
+        sans: ['Oswald', ...defaultTheme.fontFamily.sans],
+        serif: ['"EB Garamond"', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
